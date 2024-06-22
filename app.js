c7cf9e1674bcd24849a6a5f0a8191f433665f0d7
shahwaizqasim <syedshahwaiz32@gmail.com0>
2024-06-22 11:03:48 +0500

    html add

---
+++ app.js
@@ -0,0 +1,9 @@
+const column = document.querySelector(".column");
+
+const addTask = (event) => {
+    console.log(event);
+    event.preventDefault();
+}
+for (let i = 0; i < column.length; i++) {
+    console.log(column)
+}
