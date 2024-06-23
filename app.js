d224550db60285574af3143e0f50e2221c994c2b
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-06-23 08:59:17 -0700

    task add

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
-const column = document.querySelector(".column");
+const column = document.querySelectorAll(".column");
+
+const createElement = (inputValue) => {
+
+    const paragraph_Element = document.createElement("p");
+    // console.log( paragraph_Element);
+    const paragraph_Text = document.createTextNode(inputValue);
+    
+    paragraph_Element.setAttribute("draggable", "true");
+    paragraph_Element.appendChild(paragraph_Text)
+
+    // console.log(parentElement);
+
+    return paragraph_Element;
+}
+
 
 const addTask = (event) => {
-    console.log(event);
+    
     event.preventDefault();
+    console.log(event);
+    const currentForm = event.target;
+
+    const inputValue = currentForm.elements[0].value;
+    console.log(`UserName: ${inputValue}`);
+
+    const parentElement = currentForm.parentElement;
+    
+    const paragraph_Element = createElement(inputValue); 
+    console.log(paragraph_Element);
+    parentElement.insertBefore(paragraph_Element, currentForm);
+    console.log(parentElement);
+    
+    currentForm.reset();
+  
 }
+
 for (let i = 0; i < column.length; i++) {
-    console.log(column)
+    const form = column[i].lastElementChild;
+    // console.log(form);
+
+    form.addEventListener("submit", addTask)
 }
