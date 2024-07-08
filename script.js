b97dfb240fa692831a1a9a19d78ac253eaef0e45
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-07-08 11:14:56 -0700

    create new file functions.js all functions put in new file

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+
+import { createCard, createElementParagraph } from "./functions.js";
+
 // const columns = document.querySelector(".column");
-const parent_main = document.querySelector("#main");
+const main = document.querySelector("#main");
 const addCardBtn = document.querySelector("#addCard");
 
-let theElementRaised = null;
+
+
+let UserSaveTasks = JSON.parse(localStorage.getItem("savedTasks")); // local storage me se data ko fetch kar rahe hain savedTasks ko object me convert kar rahe hain
+// console.log(savedTasks);
+
+if (!UserSaveTasks) {  
+    UserSaveTasks = {}; // agr local storage me kuch bi store na ho tw empty object ko set kar do 
+}
+
 
 // savedtask pehly se local storage me save hai just column me display karwaya hai 
 // for (let i = 0; i < savedTasks.length; i++) {
@@ -13,148 +24,15 @@ let theElementRaised = null;
 
 // }
 
-const addTask = (event) => {
-    event.preventDefault();
-
-    //    console.log(event);
-       const currentForm = event.target; // current form element
-       const myInput = currentForm.elements[0]; // input written
-       let inputValue = currentForm.elements[0].value; // value written in form's input 
-       const parentElement = currentForm.parentElement; // parent of form i.e div.column
-
-    if (!inputValue) {
-        myInput.className = "addTask1"; // agr user input empty enter kary tw input ka border red ho jaega
-    }else{
-        myInput.className = "addTask"; // agr user input empty enter kary tw input ka border red ho jaega 
-        const paragraph_Element = createElementParagraph(inputValue); // paragraph create element function call
-
-        parentElement.insertBefore(paragraph_Element, currentForm);  // paragraph added in column before the form
-
-        currentForm.reset(); // clearing form
-        
-        const h5Value = parentElement.children[0].innerText; // card ky title ka text is variable me store kiya hai 
-        
-        if (!Array.isArray(UserSaveTasks[h5Value])) {
-            UserSaveTasks[h5Value] = []; // agar array nahi hai tw empty array set krwa do because undefined me .push nho ho sakta error ayega 
-        }
-        
-        UserSaveTasks[h5Value].push(inputValue);
-        localStorage.setItem("savedTasks", JSON.stringify(UserSaveTasks)); // local storage me data ko save kara hai
-    }
-}
 
 // for (let i = 0; i < columns.length; i++) {
 //     const form = columns[i].lastElementChild; // selecting every column's form because form is last element
 //     form.addEventListener("submit", addTask);
 // }
 
-const createCard = (cardsTitle) => {
-
-    // <div class="column">
-    //     <h5>Work Todo <i class="fa-solid fa-xmark"></i></h5>
-    //      <p>task 1</p>
-    //         <p>task 2 <i class="fa-solid fa-trash"></i></p>
-    //         <p>task 3</p> 
-    //     <form>
-    //       <input type="text" placeholder="add task" class="add_task" />
-    //     </form>
-    //   </div>
-
-    const myDiv = document.createElement("div")
-    myDiv.setAttribute("class", "column");
-    // console.log(myDiv);
-
-    const h6 = document.createElement("h6");
-    h6.setAttribute("class","columnTitle");
-    const h6Text = document.createTextNode(cardsTitle);
-
-    const XMarkIcon = document.createElement("i");
-    XMarkIcon.classList.add("fa-solid");
-    XMarkIcon.classList.add("fa-xmark");
-    XMarkIcon.classList.add("removeCard")
-
-    const myForm = document.createElement("form");
-
-    const myInput = document.createElement("input");
-    myInput.setAttribute("type","text");
-    myInput.setAttribute("placeholder","add task");
-    myInput.setAttribute("class","addTask");
-    
-    const myPara = document.createElement("p");
-
-    h6.appendChild(h6Text);
-    h6.appendChild(XMarkIcon);
-    myDiv.appendChild(h6);
-    myForm.appendChild(myInput);
-    myDiv.appendChild(myForm);
-
-    myForm.addEventListener("submit", addTask);
-
-    myDiv.addEventListener("dragleave", (event) => event.preventDefault());
-
-    myDiv.addEventListener("dragover", (event) => event.preventDefault());
-
-    myDiv.addEventListener("drop", (event) => {
-        let DropTicketInColumn = event.target; // jis element par drop kiya ja raha ho
-
-        const condition1 = DropTicketInColumn.className.includes("column");
-        const condition2 = DropTicketInColumn.className.includes("columnTitle");
-        const condition3 = DropTicketInColumn.className.includes("taskTicket");
-        const condition4 = DropTicketInColumn.className.includes("addTask");
-
-        if (condition1) {
-            DropTicketInColumn.insertBefore(theElementRaised, myForm);
-            console.log('column', DropTicketInColumn);
-        }
-        if (condition2) {
-            DropTicketInColumn.insertBefore(theElementRaised, myForm);
-        }
-        if (condition3) {
-            DropTicketInColumn.parentElement.insertBefore(theElementRaised, myForm);
-        }
-        if (condition4) {
-            DropTicketInColumn.parentElement.parentElement.insertBefore(theElementRaised, DropTicketInColumn.lastElementChild);
-            console.log('condition4', condition4);
-        }
-    })
-
-    return myDiv;
-    
-}
-// createCard(); 
-
-
-/* create elements in javascript */
-const createElementParagraph = (inputValue) => {
-    const paragraph_Element = document.createElement("p");
-    paragraph_Element.setAttribute("class", "taskTicket")
-    const paragraph_Text = document.createTextNode(inputValue);
-    paragraph_Element.appendChild(paragraph_Text);
-    paragraph_Element.setAttribute("draggable", "true")
-
-    const trashIcon = document.createElement('i');
-    trashIcon.classList.add('fa-solid');
-    trashIcon.classList.add("fa-trash");
-    trashIcon.classList.add('remove_Element');
-    paragraph_Element.appendChild(trashIcon);
-
-    paragraph_Element.addEventListener("mousedown", (event) => {
-        theElementRaised = event.target; // wo element jo utha hoa hai
-        console.log('theElementRaised', theElementRaised);
-        // console.log(event);
-    })
-
-    return paragraph_Element;
-}
 
 
 
-let UserSaveTasks = JSON.parse(localStorage.getItem("savedTasks")); // local storage me se data ko fetch kar rahe hain savedTasks ko object me convert kar rahe hain
-// console.log(savedTasks);
-
-if (!UserSaveTasks) {  
-    UserSaveTasks = {}; // agr local storage me kuch bi store na ho tw empty object ko set kar do 
-}
 
 /* page referesh karny par value apni jaga par rahengi */
 for (const myKey in UserSaveTasks) { // local storage se data ko display krwany ky liye forin loop ka use kiya
@@ -179,6 +57,10 @@ addCardBtn.addEventListener("click", () => {  // jb card par click ho
 })
 
 /* data structure of local storage */
+// {
+//     "workTodo" : ["task1", "task2"],
+//     "progress" : ["task1", "task2"],
+// }
 
 
 /* Remove Column for user Xmark Button use */
